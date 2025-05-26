4af2a6a35d3c4f56f1266e7b018a2f83c33b790b
kemilisantos <133013015+kemilisantos@users.noreply.github.com>
2025-05-26 10:44:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+function criaCartao(categoria,pergunta,resposta0{
+  let container= documento.getElementByld(´container´)
+  let cartao= document.creatElement('article´)
+  cartao.className='cartao'
+  cartao.innerHTML=
+  <div class="cartao__conteudo">
+  <h3>${categoria}</h3>
+  <div class="cartao__conteudo__pergunta">
+    </p>${pergunta}</p>
+  </div>
+  <div class="cartao__conteudo__resposta">
+    </div>
+    </div>
+  let respostaEstaVisivel= false
+  function viraCartao(){
+    respostaEstaVisivel=!respostaEstaVisivel
+    cartao.classList.toggle(`active',respostaEstaVisivel)
+ }
+ cartao.addEventListener(`click', viraCartao)
 
+ container.appendChild(cartao)
+
+ }
